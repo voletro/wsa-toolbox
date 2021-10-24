4e76a4d46f2c6275819f37f5e54fedea92155c53
voletro <65332602+voletro@users.noreply.github.com>
2021-10-25 09:55:42 +1100

    Update WSAInstallAlt.bat

--- WSAInstallAlt.bat
+++ WSAInstallAlt.bat
@@ -1,2 +1,2 @@
 @echo off
-start https://www.microsoft.com/en-us/p/windows-subsystem-for-android/9p3395vx91nr?activetab=pivot:regionofsystemrequirementstab
+start https://github.com/voletro/wsa-toolbox/blob/main/installation.md
