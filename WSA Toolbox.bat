0895acfc19754a0a21133f940814f9283fb1ca86
voletro <65332602+voletro@users.noreply.github.com>
2021-10-26 08:36:46 +1100

    Create WSA Toolbox.bat

---
+++ WSA Toolbox.bat
@@ -0,0 +1,15 @@
+@echo off
+@echo off
+set arg1=%1
+IF [%1]==[] (
+start wsatoolbox.hta
+exit
+) ELSE (
+echo Installing...
+adb.exe disconnect
+adb.exe connect 127.0.0.1:58526
+adb.exe install %arg1%
+echo The APK File has been installed. Shortcut Available in the Start Menu.
+pause
+)
+
