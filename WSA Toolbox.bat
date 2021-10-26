b051874c182e828d3df1e90bc1ed5c981563461d
voletro <65332602+voletro@users.noreply.github.com>
2021-10-27 09:56:17 +1100

    Edited images

--- WSA Toolbox.bat
+++ WSA Toolbox.bat
@@ -5,11 +5,5 @@ set arg1=%1
 IF [%1]==[] (
 wsatoolbox.hta
 ) ELSE (
-echo Installing...
-adb.exe disconnect
-adb.exe connect 127.0.0.1:58526
-adb.exe install %arg1%
-echo The APK File has been installed. Shortcut Available in the Start Menu.
-pause
+installAPK.bat "%arg1%"
 )
-
